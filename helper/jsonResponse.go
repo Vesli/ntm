0b7ee6821f2f502248f064857b5640d7b64a93af
vesli <passep_j@epitech.eu>
2017-05-25 16:52:42 +0100

    Updating the write json function to fit a more generic use

--- helper/jsonResponse.go
+++ helper/jsonResponse.go
@@ -7,14 +7,7 @@ import (
 
 // WriteJSON is use to write a JSON response (error and success) from object on request
 func WriteJSON(w http.ResponseWriter, obj interface{}, responseStatus int) {
-	jData, err := json.Marshal(obj)
-	if err != nil {
-		w.WriteHeader(responseStatus)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(responseStatus)
-	w.Write(jData)
+	json.NewEncoder(w).Encode(obj)
 }
